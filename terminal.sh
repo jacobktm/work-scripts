029f607200660c82258873919c6aecc7a84dd609
Jacob Kaulike <jacob.k.tm@gmail.com>
2024-07-30 08:02:30 -0600

    determine terminal based on session instead of os

--- terminal.sh
+++ terminal.sh
@@ -2,26 +2,44 @@
 
 ARGS=""
 
-if [ $# -gt 0 ];
-then
+if [ $# -gt 0 ]; then
     ARGS="$@ "
 fi
 
 # Detect the OS
 os_name=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
+session=$(echo $XDG_SESSION_DESKTOP)
+
+# Function to choose terminal emulator based on session
+choose_terminal_by_session() {
+    case "$session" in
+        gnome|ubunt|pop)
+            terminal="gnome-terminal"
+            ;;
+        kde|plasma)
+            terminal="konsole"
+            ;;
+        xfce)
+            terminal="xfce4-terminal"
+            ;;
+        mate)
+            terminal="mate-terminal"
+            ;;
+        *)
+            terminal="xterm"
+            ;;
+    esac
+}
 
 # Choose the terminal emulator based on the OS
 case "$os_name" in
-    ubuntu|pop|fedora)
-        ./install.sh gnome-terminal
-        echo "gnome-terminal ${ARGS}--"
-        ;;
-    arch)
-        ./install.sh konsole
-        echo "konsole --separate -e"
+    debian|ubuntu|pop|fedora|arch)
+        choose_terminal_by_session
+        ./install.sh $terminal
+        echo "$terminal ${ARGS}--"
         ;;
     *)
-        echo ""
+        echo "Unsupported OS"
         exit 1
         ;;
 esac

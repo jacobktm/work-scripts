6dea5a2e00a93f23dd0586955a64170280b0e4ed
Jacob Kaulike <jacob.k.tm@gmail.com>
2025-09-24 11:53:14 -0600

    Add BusyBox test script to verify successful boot after updates

---
+++ busybox_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# The BusyBox test for the update_test.sh script just echoes PASSED because if we've
+# reached this point, the system didn't fail to boot after the update.
+echo "PASSED"
+exit 0
